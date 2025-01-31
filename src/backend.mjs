8838d3db69543f3b69bb216bf73872633f205b2e
PierreML-06 <pierre.mouilleseaux-lhuillier@edu.univ-fcomte.fr>
2025-01-31 09:29:30 +0100

    backend fonctionel

--- src/backend.mjs
+++ src/backend.mjs
@@ -1,19 +1,19 @@
 import PocketBase from 'pocketbase';
 
-const db = new PocketBase("http://127.0.0.1:8090/_/#/collections?collection=pbc_3458190115&filter=&sort=-%40rowid");
+const db = new PocketBase("http://127.0.0.1:8090/");
 
 export async function getOffres() {
     try {
         let data = await db.collection('maison').getFullList({
             sort: '-created',
         });
-      //  data = ??.map((??) => {
+        //  data = ??.map((??) => {
         //    ??= ?? (??, ??);
-       // return ??;
-   // });
-    return data;
-} catch (error) {
-    console.log('Une erreur est survenue en lisant la liste des maisons', error);
-    return [];
-}
+        // return ??;
+        // });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
 }
