d243c5f610e3245436aeb71ddd6482928fa6bcb1
PierreML-06 <pierre.mouilleseaux-lhuillier@edu.univ-fcomte.fr>
2025-02-07 09:56:35 +0100

    commit fin

--- src/backend.mjs
+++ src/backend.mjs
@@ -28,3 +28,10 @@ export async function getOffre(id) {
         return null;
     }
 }
+
+export async function bySurface(surface) {
+    const records = await pb.collection('maison').getFullList({
+        filter: `surface > ${surface}`
+    });
+    return records;
+}
